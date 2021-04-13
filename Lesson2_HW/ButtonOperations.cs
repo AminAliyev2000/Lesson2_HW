d90d4da650f939a4d734c594af70e673f6e95352
Amin Aliyev <aliyevamin50@gmail.com>
2021-04-13 11:09:01 +0400

    Add project files.

---
+++ Lesson2_HW/ButtonOperations.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_HW
+{
+    public class ButtonOperations
+    {
+        public string FirstValue { get; set; }
+        public string SecondValue { get; set; }
+        public OperationType OperationType { get; set; }
+        public string Result { get; set; }
+
+        public ButtonOperations()
+        {
+            FirstValue = String.Empty;
+            SecondValue = String.Empty;
+        }
+
+        public void Clear()
+        {
+            FirstValue = String.Empty;
+            SecondValue = String.Empty;
+            Result = String.Empty;
+        }
+        public string Calculate()
+        {
+            if (String.IsNullOrWhiteSpace(FirstValue))
+                return String.Empty;
+
+            if (String.IsNullOrWhiteSpace(SecondValue))
+                return FirstValue;
+
+
+            var firstValue = Double.Parse(FirstValue);
+            var secondValue = Double.Parse(SecondValue);
+            double result;
+            switch (OperationType)
+            {
+                case OperationType.Procent:
+                    result = (firstValue * secondValue) / 100;
+                    break;
+                case OperationType.Add:
+                    result = firstValue + secondValue;
+                    break;
+                case OperationType.Subtract:
+                    result = firstValue - secondValue;
+                    break;
+                case OperationType.Multiple:
+                    result = firstValue * secondValue;
+                    break;
+                case OperationType.Divide:
+                    {
+                        if (secondValue == 0)
+                            throw new DivideByZeroException("Cannot divide by zero");
+
+                        result = firstValue / secondValue;
+                    }
+                    break;
+                case OperationType.Pow2:
+                    result = firstValue * firstValue;
+                    break;
+                case OperationType.TransponireX:
+                    {
+                        if (firstValue == 0)
+                            throw new DivideByZeroException("Cannot divide by zero");
+
+                        result = 1 / firstValue;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return result.ToString(CultureInfo.InvariantCulture);
+        }
+        public OperationType GetOperationType(string operation)
+        {
+            switch (operation)
+            {
+                case "%":
+                    return OperationType.Procent;
+                case "+":
+                    return OperationType.Add;
+                case "-":
+                    return OperationType.Subtract;
+                case "x":
+                    return OperationType.Multiple;
+                case "/":
+                    return OperationType.Divide;
+                case "x^2":
+                    return OperationType.Pow2;
+                case "1/x":
+                    return OperationType.TransponireX;
+                case "=":
+                    return OperationType.Equal;
+                default:
+                    throw new InvalidOperationException("Unknown operation");
+            }
+        }
+
+    }
+    public enum OperationType
+    {
+        Procent,
+        Add,
+        Subtract,
+        Multiple,
+        Divide,
+        Pow2,
+        TransponireX,
+        Equal
+    }
+
+}
+
---
+++ Lesson2_HW/MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson2_HW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ButtonOperations _currentOperation;
+        private bool _myInput1 = true;
+        private bool _textchanged;
+        private bool _done;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _currentOperation = new ButtonOperations();
+        }
+        private void AddValueTxtbox(string value)
+        {
+            if (value != "," && txtbox.Text == "0")
+                txtbox.Text = String.Empty;
+
+            var temp = txtbox.Text + value;
+            txtbox.Text = temp.ToString(CultureInfo.InvariantCulture);
+        }
+        private void RemoveLastValueFromTextBox()
+        {
+            if (string.IsNullOrWhiteSpace(txtbox.Text))
+                return;
+
+            txtbox.Text = txtbox.Text.Remove(txtbox.Text.Length - 1, 1);
+        }
+        private void ClearInput()
+        {
+            txtbox.Text = "0";
+        }
+        private void DoOperation(string oprtype)
+        {
+            if (String.IsNullOrWhiteSpace(txtbox.Text))
+                return;
+
+            if (String.IsNullOrWhiteSpace(_currentOperation.FirstValue))
+            {
+                _currentOperation.FirstValue = txtbox.Text;
+                _myInput1 = true;
+            }
+            else if (!_done && String.IsNullOrWhiteSpace(_currentOperation.SecondValue))
+            {
+                Calculate();
+            }
+
+            _currentOperation.OperationType = _currentOperation.GetOperationType(oprtype);
+        }
+        private void Calculate()
+        {
+            if (!String.IsNullOrWhiteSpace(_currentOperation.Result))
+            {
+                _currentOperation.FirstValue = _currentOperation.Result;
+            }
+
+            _currentOperation.SecondValue = txtbox.Text;
+
+            var result = String.Empty;
+
+            try
+            {
+                result = _currentOperation.Calculate();
+            }
+            catch (Exception ex)
+            {
+                result = $"Error: {ex.Message}";
+            }
+
+            _currentOperation.Result = result;
+
+            _currentOperation.SecondValue = String.Empty;
+
+            txtbox.Text = _currentOperation.Result;
+
+            _done = true;
+            _myInput1 = true;
+        }
+        private void txtbox_textchanged(object sender, TextChangedEventArgs e)
+        {
+            _textchanged = true;
+            _done = false;
+        }
+        private void NumreicButtons_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            if (_myInput1)
+            {
+                _textchanged = false;
+                _myInput1 = false;
+                ClearInput();
+            }
+
+            AddValueTxtbox(button.Content.ToString());
+        }
+        private void OperatorChooseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button))
+                return;
+
+            DoOperation(button.Content.ToString());
+        }
+        private void DelButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_done)
+                return;
+            RemoveLastValueFromTextBox();
+            txtbox.Focus();
+        }
+        private void CEbutton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearInput();
+        }
+        private void Cbutton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearInput();
+            _currentOperation.Clear();
+        }
+        private void plusminusbutton_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbox.Text == "0")
+                return;
+
+            if (_done)
+                return;
+            var newValue = txtbox.Text.StartsWith("-") ?
+                txtbox.Text.Remove(0, 1) :
+                txtbox.Text.Insert(0, "-");
+
+            txtbox.Text = newValue.ToString(CultureInfo.InvariantCulture);
+        }
+        private void SquareRootButton_Click(object sender, RoutedEventArgs e)
+        {
+            double number;
+            var isDouble = double.TryParse(txtbox.Text, out number);
+            if (isDouble)
+            {
+              txtbox.Text =
+                    string.Format(
+                        "{0}{1} = {2}",
+                        "\u221A",
+                        this.txtbox.Text,
+                        Math.Round(Math.Sqrt(number), 2));
+            }
+        }
+        private void equalbutton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_done)
+                Calculate();
+        }
+        private void dotbutton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!txtbox.Text.Contains(","))
+            {
+                AddValueTxtbox(",");
+            }
+        }
+    }
+}
